465cf7e7ad12c7a827c1a0fa8b0aa1bf11b0a877
tn-als <kusumin38@gmail.com>
2026-01-26 20:43:56 +0900

    refactor: refactor uvicorn

--- ecosystem.ai.config.js
+++ ecosystem.ai.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "fastapi-app",
       cwd: "/home/ubuntu/opt/ai-prod/app",
-      script: "/home/ubuntu/opt/ai-prod/app/.venv/bin/uvicorn",
-      args: "main:app --host 0.0.0.0 --port 8000",
+      script: "/home/ubuntu/opt/ai-prod/app/.venv/bin/python",
+      args: "-m uvicorn main:app --host 0.0.0.0 --port 8000",
       env: {
         PYTHONUNBUFFERED: "1",
       },
